b112c3be5e5b51d7c997362f2a7adffb4109a5e9
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2025-09-12 15:05:49 -0300

    feat: enable database connection and update logging format in DatabaseService

--- src/infrastructure/database/database.service.ts
+++ src/infrastructure/database/database.service.ts
@@ -1,23 +1,23 @@
-import { sequelize } from "@sequelize";
+import { sequelize } from "@/sequelize";
 
 export class DatabaseService {
   async connect() {
     await sequelize
       .authenticate()
       .then(() => {
-        console.log("Conexão estabelecida com sucesso");
+        console.log("[debug] Conexão estabelecida com sucesso");
       })
       .catch((error) => {
-        console.error("Erro ao conectar ao banco de dados:", error);
+        console.error("[error] Erro ao conectar ao banco de dados:", error);
       });
 
     await sequelize
       .sync()
       .then(() => {
-        console.log("Sincronização do banco de dados realizada com sucesso");
+        console.log("[debug] Sincronização do banco de dados realizada com sucesso");
       })
       .catch((error) => {
-        console.error("Erro ao sincronizar o banco de dados:", error);
+        console.error("[error] Erro ao sincronizar o banco de dados:", error);
       });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { App } from "./app";
 import { config } from "./infrastructure/config";
-// import { DatabaseService } from "./infrastructure/database/database.service";
+import { DatabaseService } from "./infrastructure/database/database.service";
 
 async function bootstrap() {
-  // await new DatabaseService().connect();
+  await new DatabaseService().connect();
 
   const app = new App();
   app.start(config.PORT);

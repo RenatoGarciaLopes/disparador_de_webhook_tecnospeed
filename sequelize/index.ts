2eb3b3738b490c9d2d225272b72d3295a8bf8c9f
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2025-09-12 20:56:18 -0300

    feat: switch to sequelize-typescript and configure model loading with custom matching logic

--- sequelize/index.ts
+++ sequelize/index.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 const config = require("./config");
 
 const configEnv = config[process.env.NODE_ENV || "development"];
@@ -6,4 +6,12 @@ const configEnv = config[process.env.NODE_ENV || "development"];
 export const sequelize = new Sequelize({
   ...configEnv,
   logging: false,
+  models: [__dirname + "/models/**/*.model.ts"],
+  modelMatch: (filename, member) => {
+    // transform <model-name>.model.ts to <modelname> for match with model name in file (e.g. ModelName)
+    return (
+      filename.substring(0, filename.indexOf(".model")).replace("-", "") ===
+      member.toLowerCase()
+    );
+  },
 });

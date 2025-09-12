62eb3247365a5c7770fd803f0af5f280420aa0c4
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2025-09-12 20:56:02 -0300

    test: add unit tests for DatabaseService to verify connection and synchronization behavior

--- src/infrastructure/database/database.service.ts
+++ src/infrastructure/database/database.service.ts
@@ -1,25 +1,36 @@
 import { sequelize } from "@/sequelize";
 
+
 export class DatabaseService {
-  async connect() {
-    await sequelize
+  async connect(): Promise<boolean> {
+    const cantAuthenticate = await sequelize
       .authenticate()
       .then(() => {
         console.log("[debug] Conexão estabelecida com sucesso");
+        return false;
       })
       .catch((error) => {
         console.error("[error] Erro ao conectar ao banco de dados:", error);
+        return true;
       });
 
-    await sequelize
+    if (cantAuthenticate) return false;
+
+    const cantSync = await sequelize
       .sync()
       .then(() => {
         console.log(
           "[debug] Sincronização do banco de dados realizada com sucesso",
         );
+        return false;
       })
       .catch((error) => {
         console.error("[error] Erro ao sincronizar o banco de dados:", error);
+        return true;
       });
+
+    if (cantSync) return false;
+
+    return true;
   }
 }

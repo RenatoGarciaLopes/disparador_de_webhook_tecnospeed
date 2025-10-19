ad337cf7db3ec5147b10739157ac5f1c0e4fa497
Olival42 <phsolival@gmail.com>
2025-10-19 14:18:00 -0300

    feat: implementei o controller do protocolo

--- src/modules/protocolo/domain/services/GetProtocolosService.ts
+++ src/modules/protocolo/domain/services/GetProtocolosService.ts
@@ -1,15 +1,19 @@
 import { GetProtocolosUseCase } from "../../application/use-cases/protocolo/GetProtocolosUseCase";
-import { ProtocoloParamSchemaDTO } from "../../interfaces/http/validators/ProtocoloParamSchema";
+import { GetProtocoloByIdUseCase } from "../../application/use-cases/protocolo/GetProtocoloByIdUseCase";
 import { ProtocolosSchemaDTO } from "../../interfaces/http/validators/ProtocolosSchema";
+import { ProtocoloParamSchemaDTO } from "../../interfaces/http/validators/ProtocoloParamSchema";
 
 export class GetProtocolosService {
-  constructor(private readonly getProtocoloUseCase: GetProtocolosUseCase) {}
+  constructor(
+    private readonly getProtocolosUseCase: GetProtocolosUseCase,
+    private readonly getProtocoloByIdUseCase: GetProtocoloByIdUseCase,
+  ) {}
 
   async getProtocolos(cedenteId: number, data: ProtocolosSchemaDTO) {
-    return [];
+    return this.getProtocolosUseCase.execute(data, cedenteId);
   }
 
   async getProtocoloById(cedenteId: number, data: ProtocoloParamSchemaDTO) {
-    return [];
+    return this.getProtocoloByIdUseCase.execute(data, cedenteId);
   }
 }
--- src/modules/protocolo/interfaces/http/controllers/ProtocolosController.ts
+++ src/modules/protocolo/interfaces/http/controllers/ProtocolosController.ts
@@ -1,31 +1,63 @@
 import { Request, Response } from "express";
 import { ProtocolosSchemaDTO } from "../validators/ProtocolosSchema";
 import { ProtocoloParamSchemaDTO } from "../validators/ProtocoloParamSchema";
+import { InvalidFieldsError } from "@/shared/errors/InvalidFields";
+import { GetProtocolosService } from "../../../domain/services/GetProtocolosService";
+import { validateBody } from "../middlewares/protocolo/validate-body";
 
 export class ProtocolosController {
+  constructor(private service: GetProtocolosService) {}
+
   public async getProtocolos(
     req: Request<{}, {}, ProtocolosSchemaDTO> & { cedenteId: number },
     res: Response,
   ) {
     try {
-      res.status(200).json({});
+      const bodyValidated = await validateBody(req.body);
+      const protocolos = await this.service.getProtocolos(
+        req.cedenteId,
+        bodyValidated,
+      );
+      return res.status(200).json(protocolos);
     } catch (error) {
-      res.status(500).json({
-        message: "Erro interno do servidor",
-      });
+      if (error instanceof InvalidFieldsError) {
+        return res
+          .status(400)
+          .json({ error: error.error, message: error.code });
+      }
+      return res.status(500).json({ message: "Erro interno do servidor" });
     }
   }
 
   public async getProtolocoById(
-    req: Request<{}, {}, ProtocoloParamSchemaDTO> & { cedenteId: number },
+    req: Request<{ id: string }, {}, ProtocoloParamSchemaDTO> & {
+      cedenteId: number;
+    },
     res: Response,
   ) {
     try {
-      res.status(200).json({});
+      const { id } = req.params;
+      const { cedenteId } = req;
+
+      if (!id) {
+        return res.status(400).json({ message: "UUID é obrigatório" });
+      }
+
+      const uuidRegex =
+        /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+      if (!uuidRegex.test(id)) {
+        return res.status(400).json({ message: "UUID inválido" });
+      }
+
+      const protocolo = await this.service.getProtocoloById(cedenteId, { id });
+
+      if (!protocolo) {
+        return res.status(400).json({ message: "Protocolo não encontrado." });
+      }
+
+      return res.status(200).json(protocolo);
     } catch (error) {
-      res.status(500).json({
-        message: "Erro interno do servidor",
-      });
+      return res.status(500).json({ message: "Erro interno do servidor" });
     }
   }
 }
